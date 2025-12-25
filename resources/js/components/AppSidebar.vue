45d9cf47f02f5b69fedb9e91e7f9a8981a91396b
madhusudhan1234 <madhusudhansubedi4@gmail.com>
2025-12-25 22:55:42 +0900

    restrict for the dashboard stuff

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -14,7 +14,7 @@ import {
 import { dashboard, episodesDashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, Headphones, LayoutGrid } from 'lucide-vue-next';
+import { Headphones, LayoutGrid } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -31,16 +31,7 @@ const mainNavItems: NavItem[] = [
 ];
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
+    
 ];
 </script>
 
