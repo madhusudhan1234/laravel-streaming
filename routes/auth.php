7b139865f043cfab829e080e11e8c5f4dd52a99b
madhusudhan1234 <madhusudhansubedi4@gmail.com>
2026-02-04 03:49:46 +0900

    logout fix

--- routes/auth.php
+++ routes/auth.php
@@ -14,8 +14,3 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 });
-
-Route::middleware('auth')->group(function () {
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name('logout');
-});
